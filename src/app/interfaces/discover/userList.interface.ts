0cfae5e4d219952d1593147833208836882cf828
Brayan Garzon <destroyergarzon@gmail.com>
2023-08-05 23:33:59 -0500

    vista finalizada lista de comentarios de usuarios

--- src/app/interfaces/discover/userList.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./user.interface";
-
-
-
-export interface usersList {
-    id: number;
-    name: User;
-    email: User;
-}
--- src/app/pages/info-user-comment/info-user-comment.component.ts
+++ src/app/pages/info-user-comment/info-user-comment.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Helper } from 'src/app/helper/helper';
 import { CommentsService, UserService } from 'src/app/services/discover';
 
 
@@ -13,6 +14,14 @@ export class InfoUserCommentComponent {
 
   user:any
   sites:any
+  // Función para obtener el texto del botón dinámicamente content: "\2B50";
+getButtonText(comment: any): string {
+  return comment.expanded ? 'Ver menos' : 'Ver más';
+}
+
+calculateMissingStars(quality: number | undefined): number {
+  return 5 - (quality || 0);
+}
 
   constructor(private route:ActivatedRoute,  private userService: UserService, private commentService:CommentsService){
     this.userService.getUserById(parseInt(this.route.snapshot.queryParamMap.get('user')!)).subscribe( res =>{
@@ -25,4 +34,8 @@ export class InfoUserCommentComponent {
       this.sites = res
     } )
   }
+  getNumberRange(end: any) {
+    return Helper.getNumberRange(1, end);
+  }
+ 
 }
